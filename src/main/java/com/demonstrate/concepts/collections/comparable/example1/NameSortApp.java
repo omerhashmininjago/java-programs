d658701da42660cc8df98b6b70bce653e22546bc
omerhashmininjago <hashmi.omer@gmail.com>
2019-02-20 00:28:25 +0530

    Examples for Comparable

---
+++ src/main/java/com/demonstrate/concepts/collections/comparable/example1/NameSortApp.java
@@ -0,0 +1,22 @@
+package com.demonstrate.concepts.collections.comparable.example1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class NameSortApp {
+
+    public static void main(String[] args) {
+
+        Name nameArray[] = {
+                new Name("John", "Smith"),
+                new Name("Karl", "Ng"),
+                new Name("Jeff", "Smith"),
+                new Name("Tom", "Rich")
+        };
+
+        List<Name> names = Arrays.asList(nameArray);
+        Collections.sort(names);
+        System.out.println(names);
+    }
+}
