b4400a55e1666038171bb1dbf6cd8e7a636c9929
omerhashmininjago <hashmi.omer@gmail.com>
2019-02-26 01:15:41 +0530

    Immutable example

--- src/main/java/com/demonstrate/concepts/enumerations/example2/Calculations.java
+++ src/main/java/com/demonstrate/concepts/enumerations/example2/Calculations.java
@@ -4,6 +4,9 @@ package com.demonstrate.concepts.enumerations.example2;
  * Here we are demonstrating how we can use Enums as Singletons
  * <p>
  * Enums are the best way to create Singletons
+ * <p>
+ * Enums cannot neither be cloned nor deserialized - Check Enum class
+ * Reflection cannot be used to destroy Enum as constructors are private
  */
 
 public enum Calculations {
